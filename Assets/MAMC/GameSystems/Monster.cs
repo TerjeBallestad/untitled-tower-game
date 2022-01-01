f143455db3b53e7f3b85a167eb2d3eef6e8f26df
Terje Ballestad <terj.ba@gmail.com>
2022-01-01 19:22:17 +0100

    Monster rage

--- Assets/MAMC/GameSystems/Monster.cs
+++ Assets/MAMC/GameSystems/Monster.cs
@@ -5,35 +5,41 @@ using UnityEngine;
 public class Monster : MonoBehaviour {
     public BlockType Type { get; private set; }
 
-    [SerializeField] private Sprite RedSprite;
-    [SerializeField] private Sprite PurpleSprite;
-    [SerializeField] private Sprite BlueSprite;
-    [SerializeField] private Sprite GreenSprite;
-    private int _rage;
+    [SerializeField] private Sprite _redSprite;
+    [SerializeField] private Sprite _purpleSprite;
+    [SerializeField] private Sprite _blueSprite;
+    [SerializeField] private Sprite _greenSprite;
+    [SerializeField] private ProgressBar _rageBar;
+    private float _rage;
     private SpriteRenderer _spriteRenderer;
     private TowerManager _tower;
 
     private void Start () {
-        _rage = 0;
         _tower = GameManager.Instance.TowerManager;
         _spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
         Setup (BlockType.green);
     }
+    private void Update () {
+        _rage += Time.deltaTime;
+        _rageBar.UpdateCurrentFill (100f, _rage, Color.red);
+    }
 
     public void Setup (BlockType type) {
+        _rage = 0;
         Type = type;
+        _rageBar.gameObject.SetActive (true);
         switch (Type) {
             case BlockType.green:
-                _spriteRenderer.sprite = GreenSprite;
+                _spriteRenderer.sprite = _greenSprite;
                 break;
             case BlockType.purple:
-                _spriteRenderer.sprite = PurpleSprite;
+                _spriteRenderer.sprite = _purpleSprite;
                 break;
             case BlockType.red:
-                _spriteRenderer.sprite = RedSprite;
+                _spriteRenderer.sprite = _redSprite;
                 break;
             case BlockType.blue:
-                _spriteRenderer.sprite = BlueSprite;
+                _spriteRenderer.sprite = _blueSprite;
                 break;
             default:
                 break;
@@ -45,11 +51,11 @@ public class Monster : MonoBehaviour {
         if (block == null || block.BeingTouched == false) return;
 
         if (block.Type != Type) {
-            _rage++;
+            _rage += 30f;
         } else {
-            _rage--;
+            _rage -= 30f;
         }
+
         block.GetEaten (this);
     }
-
 }
